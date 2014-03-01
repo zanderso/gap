b4d5df123296f84c2f589bcddcf5fad270ca17aa
Zach Anderson <zra@google.com>
2014-02-28 16:08:41 -0800

    Use vector math in AngularSpeed

--- lib/gyroscope.dart
+++ lib/gyroscope.dart
@@ -1,9 +1,11 @@
 library rikulo_gyroscope;
 
 import 'dart:html';
+import 'dart:math';
 import 'package:js/js.dart' as js;
 import 'js.dart';
 import 'device.dart';
+import 'package:vector_math/vector_math.dart';
 
 part 'src/AngularSpeed.dart';
 part 'src/Gyroscope.dart';
--- lib/src/AngularSpeed.dart
+++ lib/src/AngularSpeed.dart
@@ -3,16 +3,42 @@ part of rikulo_gyroscope;
 /** The angular speed information in radians/s.
  */
 class AngularSpeed {
-  final num x;
-  final num y;
-  final num z;
+  final Vector3 w;
   final int timestamp;
+  static final double NS2S = 1.0 / 1000000000.0;
 
-  AngularSpeed.fromProxy(js.Proxy p)
-      : this.x = p.x,
-        this.y = p.y,
-        this.z = p.z,
-        this.timestamp = p.timestamp;
+  AngularSpeed.fromProxy(js.Proxy p) {
+    this.w = new Vector3(p.x, p.y, p.z);
+    this.timestamp = p.timestamp;
+  }
+    
+  Quaternion _quaternion(int old_ts) {
+    if (old_ts != 0) {
+      double dT = (this.timestamp.toDouble() - ts.toDouble()) * NS2S;
 
-  String toString() => "($timestamp: $x, $y, $z)";
+      double w_mag = sqrt(w.x*w.x + w.y*w.y + w.z*w.z);
+      Vector3 w_norm = this.w / w_mag;
+      if (w_norm < 0.0001) {
+        return null;
+      }
+
+      double thetaOverTwo = w_mag * dT / 2.0;
+      double sinThetaOverTwo = sin(thetaOverTwo);
+      double cosThetaOverTwo = cos(thetaOverTwo);
+      return new Quaternion(sinThetaOverTwo * w_norm.x,
+                            sinThetaOverTwo * w_norm.y,
+                            sinThetaOverTwo * w_norm.z,
+                            cosThetaOverTwo);
+    }
+    return null;
+  }
+
+  void doRotation(Vector3 v, int old_ts) {
+    Quaternion q = _quaternion(old_ts);
+    if (q != null) {
+      q.rotate(v);
+    }
+  }
+
+  String toString() => "($timestamp: ${w.x}, ${w.y}, ${w.z})";
 }
